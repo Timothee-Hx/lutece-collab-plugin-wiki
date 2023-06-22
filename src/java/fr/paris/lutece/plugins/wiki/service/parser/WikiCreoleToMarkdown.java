44791cb271d7fd045e3e540857555041bb2c1d1a
timothee-hx <timothee.harleaux@atos.net>
2023-06-22 17:38:31 +0200

    parse html

--- src/java/fr/paris/lutece/plugins/wiki/service/parser/WikiCreoleToMarkdown.java
+++ src/java/fr/paris/lutece/plugins/wiki/service/parser/WikiCreoleToMarkdown.java
@@ -1,15 +1,52 @@
 package fr.paris.lutece.plugins.wiki.service.parser;
+import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
-import fr.paris.lutece.plugins.wiki.service.WikiService;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+//import an html parser
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.List;
+
 
 public class WikiCreoleToMarkdown {
     public static String wikiCreoleToMd(String strWikiText, String strPageName, String strPageUrl, String strLanguage ) {
-
+// new css class to add in context
+        ParserOptions parserOptions = new ParserOptions();
+       // parserOptions.setTocClass("wikiCreole_html_toc_class"); ne marche pas
+        parserOptions.setSizedImageClass("wikiCreole_html_imageSize_class");
         String htmlContent = new LuteceWikiParser(  strWikiText,  strPageName,  strPageUrl,  strLanguage  ).toString( );
 
-        System.out.println("htmlContent____________________________\n" + htmlContent);
-        String markdown = FlexmarkHtmlConverter.builder().build().convert(htmlContent);
-        System.out.println("markdown____________________________\n" + markdown);
+
+        // parse input HTML
+        Document htmlDocument = Jsoup.parse(htmlContent);
+        // list all the classes in the html
+        List<Element> elements = htmlDocument.getAllElements();
+        for (Element element : elements) {
+            System.out.println("element.class: " + element.className());
+            if(element.className().equals("well")){
+                element.remove();
+                // add a new element at the beginning of the document
+                // markdown to add
+                String toc = "<div>\n";
+                 toc += "$$\n";
+                toc += "<span class ='toc'>Table des matières</span>\n";
+                toc += "\n$$\n";
+                toc += "</div>\n";
+                htmlDocument.prepend(toc);
+            }
+
+
+        }
+        System.out.println("htmlContent____________________________\n" + htmlDocument.toString());
+
+
+        MutableDataSet options = new MutableDataSet();
+        options.set(HtmlRenderer.SOFT_BREAK, "\n");
+                String markdown = FlexmarkHtmlConverter.builder(options).build().convert(htmlContent);
+
+
 
         return markdown;
     }
