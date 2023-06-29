1ea6c2e9dd68735cd93b77867e16a3a5cecb7f90
timothee-hx <timothee.harleaux@atos.net>
2023-06-29 12:27:24 +0200

    fix update sql scripts and remove modifications made by mistake to init scripts

--- src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
+++ src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
@@ -1,3 +1,7 @@
-ALTER TABLE wiki_topic ADD COLUMN parent_page_name VARCHAR(100) DEFAULT '' NOT NULL;
-ALTER TABLE wiki_topic_version ADD COLUMN is_published INT DEFAULT '0' NOT NULL;
-UPDATE wiki_topic_version wtv1 SET is_published = 1 WHERE id_topic_version = (SELECT MAX(id_topic_version) FROM wiki_topic_version wtv2 WHERE wtv1.id_topic = wtv2.id_topic);
+ALTER TABLE wiki_topic_version ADD COLUMN is_published INT DEFAULT '1' NOT NULL;
+ALTER TABLE wiki_topic_version MODIFY is_published INT DEFAULT '0' NOT NULL;
+ALTER TABLE wiki_topic ADD modify_page_last_open_by VARCHAR(100);
+ALTER TABLE wiki_topic ADD modify_page_last_open_at TIMESTAMP;
+ALTER TABLE wiki_topic_version_content ADD html_wiki_content LONG VARCHAR NULL;
+
+
