424a25b1baf7bba72b5fccd78e11892ba2ddfcf9
timothee-hx <timothee.harleaux@atos.net>
2023-07-19 13:57:44 +0200

    25131 Modernize the markup language used 24954 Title styles 25169 create an option draft to save the work in progress

--- src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
+++ src/sql/plugins/wiki/upgrade/update_db_wiki-3.0.1-3.0.2.sql
@@ -2,6 +2,5 @@ ALTER TABLE wiki_topic_version ADD COLUMN is_published INT DEFAULT '2' NOT NULL;
 ALTER TABLE wiki_topic_version MODIFY is_published INT DEFAULT '0' NOT NULL;
 
 ALTER TABLE wiki_topic_version_content ADD html_wiki_content LONG VARCHAR NULL;
-ALTER TABLE wiki_topic_version_content CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
 
 
